68386a38705c62a74e12922e586e3e3c896cd82f
dinumerceanu <dinu.eu@yahoo.com>
2025-07-29 18:03:56 +0300

    update

--- leetcode/two_sum_1.rs
+++ leetcode/two_sum_1.rs
@@ -1,53 +1,28 @@
 use std::io;
 
-// fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-//     let mut res: Vec<i32> = Vec::new();
-
-//     let mut value_index_pairs: Vec<(i32, usize)> = nums.into_iter().enumerate().map(|(index, value)| (value, index)).collect();
-//     value_index_pairs.sort_by_key(|&(value, _index)| value);
-
-
-//     let mut left: usize = 0;
-//     let mut right: usize = value_index_pairs.len() - 1;
-
-//     while left < right {
-//         if value_index_pairs[left].0 + value_index_pairs[right].0 == target {
-//             res.push(value_index_pairs[left].1 as i32);
-//             res.push(value_index_pairs[right].1 as i32);
-//             left = right;
-//         } else if value_index_pairs[left].0 + value_index_pairs[right].0 > target {
-//             right -= 1;
-//         } else {
-//             left += 1;
-//         }
-//     }
-
-//     res
-// }
-
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut res: Vec<i32> = Vec::new();
+
     let mut value_index_pairs: Vec<(i32, usize)> = nums.into_iter().enumerate().map(|(index, value)| (value, index)).collect();
     value_index_pairs.sort_by_key(|&(value, _index)| value);
+
 
     let mut left: usize = 0;
     let mut right: usize = value_index_pairs.len() - 1;
 
     while left < right {
-        let sum = value_index_pairs[left].0 + value_index_pairs[right].0;
-        if sum == target {
-            // Am găsit o soluție. Returnăm imediat indicii originali.
-            // Ordinea indicilor (cel mai mic primul) nu e garantată aici,
-            // dar poți folosi min/max dacă e necesar.
-            return vec![value_index_pairs[left].1 as i32, value_index_pairs[right].1 as i32];
-        } else if sum > target {
+        if value_index_pairs[left].0 + value_index_pairs[right].0 == target {
+            res.push(value_index_pairs[left].1 as i32);
+            res.push(value_index_pairs[right].1 as i32);
+            left = right;
+        } else if value_index_pairs[left].0 + value_index_pairs[right].0 > target {
             right -= 1;
         } else {
             left += 1;
         }
     }
 
-    // Dacă nu s-a găsit nicio soluție, returnează un vector gol.
-    Vec::new()
+    res
 }
 
 fn main() {
