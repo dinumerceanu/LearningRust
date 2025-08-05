ed867e0341e817fc84a178570db3321f45ca88ba
dinumerceanu <dinu.eu@yahoo.com>
2025-08-05 17:40:18 +0300

    insertion sort

---
+++ SDA/sorting/src/insertion_sort.rs
@@ -0,0 +1,106 @@
+pub fn insertion_sort(a: &mut [i32]) {
+    if a.len() <= 1 {
+        return;
+    }
+
+    for i in 1..a.len() {
+        if a[i] < a[i - 1] {
+            let mut current = a[i];
+            let mut j = i;
+            while j > 0 && current < a[j - 1] {
+                a[j] = a[j - 1];
+                j -= 1;
+            }
+            a[j] = current;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_caz_normal() {
+        let mut arr = [8, 2, 4, 9, 3, 6, 1, 5, 7];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn test_vector_gol() {
+        let mut arr: [i32; 0] = [];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, []);
+    }
+
+    #[test]
+    fn test_un_singur_element() {
+        let mut arr = [42];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [42]);
+    }
+
+    #[test]
+    fn test_doua_elemente_neordonate() {
+        let mut arr = [7, 6];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [6, 7]);
+    }
+
+    #[test]
+    fn test_doua_elemente_ordonate() {
+        let mut arr = [6, 7];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [6, 7]);
+    }
+
+    #[test]
+    fn test_vector_deja_sortat() {
+        let mut arr = [1, 2, 3, 4, 5, 6];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn test_vector_sortat_invers() {
+        let mut arr = [6, 5, 4, 3, 2, 1];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn test_cu_elemente_duplicate() {
+        let mut arr = [4, 3, 3, 2, 6];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [2, 3, 3, 4, 6]);
+    }
+    
+    #[test]
+    fn test_cu_multiple_duplicate() {
+        let mut arr = [5, 2, 8, 2, 5, 8, 2, 5];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [2, 2, 2, 5, 5, 5, 8, 8]);
+    }
+
+    #[test]
+    fn test_toate_elementele_sunt_identice() {
+        let mut arr = [5, 5, 5, 5, 5];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [5, 5, 5, 5, 5]);
+    }
+
+    #[test]
+    fn test_cu_numere_negative() {
+        let mut arr = [-5, 8, -10, 0, 3, -1];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [-10, -5, -1, 0, 3, 8]);
+    }
+
+    #[test]
+    fn test_cu_pivot_la_margine() {
+        let mut arr = [10, 1, 2, 3, 4, 5];
+        insertion_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 10]);
+    }
+}
--- SDA/sorting/src/main.rs
+++ SDA/sorting/src/main.rs
@@ -1,6 +1,7 @@
 mod merge_sort;
 mod quick_sort;
 mod bubble_sort;
+mod insertion_sort;
 
 fn main() {
     // let a = vec![100, 4, 67, 46, 91, 0, 2];
@@ -13,9 +14,14 @@ fn main() {
 
     // println!("{:?}", b);
 
-    let mut c = vec![8, 2, 4, 9, 3, 6, 1, 5, 7];
-    bubble_sort::bubble_sort(&mut c);
+    // let mut c = vec![8, 2, 4, 9, 3, 6, 1, 5, 7];
+    // bubble_sort::bubble_sort(&mut c);
 
-    println!("{:?}", c);
+    // println!("{:?}", c);
+
+    let mut d = vec![8, 2, 4, 9, 3, 6, 1, 5, 7];
+    insertion_sort::insertion_sort(&mut d);
+
+    println!("{:?}", d);
 
 }
