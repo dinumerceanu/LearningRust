dace85c82546a2ff3d08a9c081a34b0acfe3793c
dinumerceanu <dinu.eu@yahoo.com>
2025-08-05 11:32:15 +0300

    queue implementation

---
+++ SDA/lists/src/lists/queue.rs
@@ -0,0 +1,64 @@
+use std::{cell::{Ref, RefCell}, rc::Rc};
+
+struct Node {
+    val: i32,
+    next: Option<Rc<RefCell<Node>>>,
+}
+
+impl Node {
+    fn new(val: i32) -> Self {
+        Node {
+            val,
+            next: None,
+        }
+    }
+}
+
+pub struct Queue {
+    head: Option<Rc<RefCell<Node>>>,
+    tail: Option<Rc<RefCell<Node>>>,
+}
+
+impl Queue {
+    pub fn new() -> Self {
+        Queue {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push(&mut self, val: i32) {
+        let new_node = Rc::new(RefCell::new(Node::new(val)));
+        let head_node = self.head.clone();
+
+        if let Some(head) = head_node {
+            self.tail.as_ref().unwrap().borrow_mut().next = Some(Rc::clone(&new_node));
+            self.tail = Some(new_node);
+        } else {
+            self.head = Some(Rc::clone(&new_node));
+            self.tail = Some(new_node);
+        }
+        
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        let head_rc = self.head.take()?;
+
+        let val = head_rc.borrow().val;
+
+        self.head = head_rc.borrow().next.clone();
+
+        Some(val)
+    }
+
+    pub fn print_queue(&self) {
+        let mut iter = self.head.clone();
+
+        while let Some(current) = iter {
+            print!("{} ", current.borrow().val);
+            iter = current.borrow().next.clone();
+        }
+
+        println!()
+    }
+}
---
+++ SDA/lists/src/main.rs
@@ -0,0 +1,19 @@
+use crate::lists::queue::Queue;
+
+mod lists;
+
+fn main() {
+    let mut list1 = Queue::new();
+    list1.push(1);
+    list1.push(2);
+    list1.push(3);
+    list1.push(4);
+    list1.print_queue();
+    list1.pop();
+    list1.print_queue();
+    list1.pop();
+    list1.pop();
+    list1.print_queue();
+    println!("{:?}", list1.pop());
+    println!("{:?}", list1.pop());
+}
