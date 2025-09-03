c902402735fd3f8c93605f4c94a8e9e6c721ede9
dinumerceanu <dinu.eu@yahoo.com>
2025-09-03 15:07:28 +0000

    basic client-server

---
+++ async-chat-server/src/bin/client.rs
@@ -0,0 +1,66 @@
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+use std::io::Write;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let stream = TcpStream::connect("127.0.0.1:8080").await?;
+
+    println!("Connected to server!");
+
+    let (mut stream_reader, mut stream_writer) = stream.into_split();
+
+    let stdin_reader_future = async move {
+        loop {
+            print!("async-chat> ");
+            std::io::stdout().flush().unwrap();
+
+            let mut buf = vec![0; 1024];
+            let res = io::stdin().read(&mut buf).await;
+
+            if let Ok(n) = res {
+                if n < 2 {
+                    continue;
+                }
+
+                match stream_writer.write_all(&buf[..n]).await {
+                    Ok(_) => {},
+                    Err(e) => {
+                        eprintln!("Error writing to the server: {e}");
+                    }
+                }
+                
+            }
+        }
+    };
+
+    let server_reader_future = async move {
+        loop {
+            let mut buf = vec![0; 1024];
+            let res = stream_reader.read(&mut buf).await;
+
+            match res {
+                Ok(0) => {
+                    println!("Connection terminated by the server");
+                    break;
+                },
+                Ok(n) => {
+                    println!("Received from server: {:?}", &buf[..n]);
+                },
+                Err(e) => {
+                    eprintln!("Error reading from server: {e}");
+                    break;
+                }
+            } 
+        }
+    };
+
+    tokio::select! {
+        _ = stdin_reader_future => {},
+        _ = server_reader_future => {},
+    }
+
+    Ok(())
+}
---
+++ async-chat-server/src/bin/server.rs
@@ -0,0 +1,36 @@
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::TcpListener
+};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        println!("New connection from {:?}", socket.peer_addr());
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => {
+                        println!("Connection from {:?} terminated", socket.peer_addr());
+                        break;
+                    },
+                    Ok(n) => {
+                        let s = String::from_utf8_lossy(&buf[..n]);
+                        let trimmed_s = s.trim();
+                        println!("Received from {:?}: {:?}", socket.peer_addr(), trimmed_s);
+                    },
+                    Err(e) => {
+                        eprintln!("Error readinng from {:?}: {}", socket.peer_addr(), e);
+                    }
+                }
+            }
+        });
+    }
+}
